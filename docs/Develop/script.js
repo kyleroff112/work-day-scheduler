81c4a06d7a5f6469333aa30f4fd7661877e1f687
kyleroff112 <kyleroff112@gmail.com>
2023-02-09 19:21:31 -0500

    added a plethora of code such as the javascript, and changes to html

--- docs/Develop/script.js
+++ docs/Develop/script.js
@@ -1,23 +1,50 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  console.log("ready!");
+  // Get the current hour
+  const currentHour = new Date().getHours();
+
+  // Get saved data from local storage
+  const savedData = JSON.parse(localStorage.getItem("scheduleData")) || {};
+
+  // Update the description in local storage when the save button is clicked
+  $(".saveBtn").click(function () {
+    const parentBlock = $(this).parent();
+    const id = parentBlock.attr("id");
+    const textarea = parentBlock.find("textarea");
+    const description = textarea.val();
+    savedData[id] = description;
+    localStorage.setItem("scheduleData", JSON.stringify(savedData));
+  });
+
+  // Apply past, present, or future class to each time block based on the current hour
+  $(".time-block").each(function () {
+    const id = $(this).attr("id");
+    const hour = parseInt(id.split("-")[1]);
+    if (hour < currentHour) {
+      $(this).removeClass();
+      $(this).addClass("row time-block past");
+    } else if (hour === currentHour) {
+      $(this).removeClass();
+      $(this).addClass("row time-block present");
+    } else {
+      $(this).removeClass();
+      $(this).addClass("row time-block future");
+    }
+  });
+
+  // Populate textareas with saved data
+  Object.entries(savedData).forEach(([id, description]) => {
+    const textarea = $(`#${id} textarea`);
+    textarea.val(description);
+  });
+
+  // Display the current date in the header of the page
+  const currentDate = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  $("#currentDay").text(currentDate);
 });
+
